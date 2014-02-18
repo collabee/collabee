700b2b6f526794869eb0c0e08d02a8ad022a95dd
lunar- <seadlae@gmail.com>
2014-02-18 15:22:36 +0900

    landing을 index로 page2를 project로 이름 바꿈.

--- app/route.js
+++ app/route.js
@@ -1,9 +1,9 @@
-var landing = require('./controllers/landing');
-var home = require('./controllers/home');
+var index = require('./controllers/index');
+var project = require('./controllers/project');
 var workspace = require('./controllers/workspace');
 
 module.exports = function(app) {
-	app.get('/', home.index);
-	app.get('/landing', landing.index);
+	app.get('/', index.index);
 	app.get('/workspace', workspace.index);
+	app.get('/project', project.index);
 };
